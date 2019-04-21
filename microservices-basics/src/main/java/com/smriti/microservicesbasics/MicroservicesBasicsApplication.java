791bbc7dc59e89f9f6ffe15fc15421a310c2b55e
smriti.mool <smriti.mool@f1soft.com>
2019-04-21 16:17:46 +0545

    initial commit

---
+++ microservices-basics/src/main/java/com/smriti/microservicesbasics/MicroservicesBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.smriti.microservicesbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesBasicsApplication.class, args);
+	}
+
+}
