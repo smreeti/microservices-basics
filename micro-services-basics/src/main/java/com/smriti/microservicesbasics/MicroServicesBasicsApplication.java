924f8d62d91d33ac4bc6eca4a7a7a36ad522c6f9
smriti.mool <smriti.mool@f1soft.com>
2019-04-21 16:11:43 +0545

    add three spring boot projects

---
+++ micro-services-basics/src/main/java/com/smriti/microservicesbasics/MicroServicesBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.smriti.microservicesbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicesBasicsApplication.class, args);
+	}
+
+}
